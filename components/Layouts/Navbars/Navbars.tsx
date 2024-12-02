ad533296a6286cdf53a22d64c7b7943af3e039f6
rinaldiihsan <rinaldi.210170168@mhs.unimal.ac.id>
2024-12-03 00:57:10 +0700

    feat: add navbar and section 1

---
+++ components/Layouts/Navbars/Navbars.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import React, { use, useEffect, useRef, useState } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { useMediaQuery } from 'usehooks-ts';
+import { AnimatePresence, motion } from 'framer-motion';
+import Logo from '@/assets/user/logo.png';
+
+export default function Navbars() {
+  const [toggle, setToggle] = useState(false);
+  const matches = useMediaQuery('(min-width: 1024px)');
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    if (!matches) {
+      setToggle(false);
+    }
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) return null;
+
+  return (
+    <nav className="absolute top-0 z-[90] flex bg-transparent w-full">
+      <div className="w-full max-w-[85rem] py-5 lg:py-10 mx-auto lg:mx-7 2xl:mx-auto px-5 md:px-5 flex justify-between items-center">
+        {/* Logo Mobile */}
+        <div className="lg:hidden">
+          <Link href="/">
+            <Image src={Logo} alt="Logo" width={100} height={30} />
+          </Link>
+        </div>
+
+        {/* Menu Desktop */}
+        {matches && (
+          <div className="flex gap-x-12 justify-between items-center w-full">
+            <div className="flex flex-row gap-x-12">
+              <Link href="/kontak" className="text-white text-lg">
+                Kontak
+              </Link>
+              <Link href="/tentang" className="text-white text-lg">
+                Tentang
+              </Link>
+            </div>
+            {/* Logo Desktop */}
+            <div className="hidden lg:block">
+              <Link href="/">
+                <Image src={Logo} alt="Logo" width={150} height={40} className="w-40" />
+              </Link>
+            </div>
+            <div className="flex flex-row gap-x-12">
+              <Link href="/belanja" className="text-white text-lg">
+                Belanja
+              </Link>
+              <Link href="/login" className="text-white text-lg">
+                Login
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  darkMode: ['class'],
+  content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        body: 'Poppins, sans-serif',
+      },
+      colors: {},
+      container: {
+        center: true,
+        padding: '1.5rem',
+        screens: {
+          sm: '640px',
+          md: '768px',
+          lg: '1024px',
+          xl: '1240px',
+          '2xl': '1400px',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
     },
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
 export default config;
